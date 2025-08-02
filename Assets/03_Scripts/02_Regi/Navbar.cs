ea84ac92ac636b3c6f4e8d22aeb870ca0e4892cd
Cathyren Sacatani <sacatanicathyren@gmail.com>
2025-08-02 23:21:07 +0800

    AR TEST I

--- Assets/03_Scripts/02_Regi/Navbar.cs
+++ Assets/03_Scripts/02_Regi/Navbar.cs
@@ -6,16 +6,19 @@ using UnityEngine.SceneManagement;
 public class Navbar : MonoBehaviour
 {
     public GameObject settingsPanel;
-    
+
     public void BackToHome()
     {
-        // Load the scene named "02_ACCOUNT"
+        // Store which panel to open when the scene loads
+        PlayerPrefs.SetString("PanelToOpen", "Home");
+        PlayerPrefs.Save();
+
+        // Load the scene
         SceneManager.LoadScene("02_ACCOUNT");
     }
 
     public void OpenSettings()
     {
-        // Toggle the settings panel visibility
         if (settingsPanel != null)
         {
             settingsPanel.SetActive(!settingsPanel.activeSelf);
@@ -24,14 +27,11 @@ public class Navbar : MonoBehaviour
 
     public void Leaderboards()
     {
-        // Load the scene named "06_LEADERBOARD"
         SceneManager.LoadScene("06_LEADERBOARD");
     }
 
-
     public void Chatbot()
     {
-        // Load the scene named "03_SEARCH"
         SceneManager.LoadScene("03_SEARCH");
     }
 }

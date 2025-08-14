eaeeab4fbd21dd51b6a3cc1a6ad71a25d13cf732
Cathyren Sacatani <sacatanicathyren@gmail.com>
2025-08-15 01:08:07 +0800

    Event System and Connection Fix

--- Assets/03_Scripts/04_Lesson/quarters.cs
+++ Assets/03_Scripts/04_Lesson/quarters.cs
@@ -5,40 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class quarters : MonoBehaviour
 {
-    public GameObject quarter1, quarter2, quarter3, quarter4;
-
-    public void ShowQ1()
-    {
-        quarter1.SetActive(true);
-        quarter2.SetActive(false);
-        quarter3.SetActive(false);
-        quarter4.SetActive(false);
-    }
-    public void ShowQ2()
-    {
-        quarter1.SetActive(false);
-        quarter2.SetActive(true);
-        quarter3.SetActive(false);
-        quarter4.SetActive(false);
-    }
-    public void ShowQ3()
-    {
-        quarter1.SetActive(false);
-        quarter2.SetActive(false);
-        quarter3.SetActive(true);
-        quarter4.SetActive(false);
-    }
-    public void ShowQ4()
-    {
-        quarter1.SetActive(false);
-        quarter2.SetActive(false);
-        quarter3.SetActive(false);
-        quarter4.SetActive(true);
-    }
+    
     public void LoadQ1()
     {
         SceneManager.LoadScene("04_LESSON AR");
     }
 
-  
+    public void LoadQ2()
+    {
+        SceneManager.LoadScene("04_LESSONQ2");
+    }
+    public void LoadQ3()
+    {
+        SceneManager.LoadScene("04_LESSONQ3");
+    }
+    public void LoadQ4()
+    {
+        SceneManager.LoadScene("04_LESSONQ4");
+    }
+
+
 }

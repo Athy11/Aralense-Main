567b7051c466e2406b2291fca355bfa720fe15fb
Cathyren Sacatani <sacatanicathyren@gmail.com>
2025-08-15 03:17:17 +0800

    Script Connection

--- Assets/03_Scripts/05_Quiz/QuizConn.cs
+++ Assets/03_Scripts/05_Quiz/QuizConn.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QuizConn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject quarter1, quarter2, quarter3, quarter4;
+
+    public void ShowQ1()
     {
-        
+        quarter1.SetActive(true);
+        quarter2.SetActive(false);
+        quarter3.SetActive(false);
+        quarter4.SetActive(false);
+    }
+    public void ShowQ2()
+    {
+        quarter1.SetActive(false);
+        quarter2.SetActive(true);
+        quarter3.SetActive(false);
+        quarter4.SetActive(false);
+    }
+    public void ShowQ3()
+    {
+        quarter1.SetActive(false);
+        quarter2.SetActive(false);
+        quarter3.SetActive(true);
+        quarter4.SetActive(false);
+    }
+    public void ShowQ4()
+    {
+        quarter1.SetActive(false);
+        quarter2.SetActive(false);
+        quarter3.SetActive(false);
+        quarter4.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CloseQ1()
     {
-        
+        quarter1.SetActive(false);
+    }
+    public void CloseQ2()
+    {
+        quarter2.SetActive(false);
+    }
+    public void CloseQ3()
+    {
+        quarter3.SetActive(false);
+    }
+    public void CloseQ4()
+    {
+        quarter4.SetActive(false);
     }
 }

056296c78454a1e4f7189d27624b68f158a4feb5
Cathyren Sacatani <sacatanicathyren@gmail.com>
2025-08-20 05:27:57 +0800

    SIMULATION UI

---
+++ Assets/03_Scripts/04_Lesson/sims_conn.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sims_conn : MonoBehaviour
+{
+    public void LoadSims()
+    {
+        SceneManager.LoadScene("07_SIMULATION AR");
+    }
+}
